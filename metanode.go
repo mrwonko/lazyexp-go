b8a6fb487d4e4c6feafc075b971ca4b8064db23e
Willi Schinmeyer <willi.schinmeyer@freiheit.com>
2017-05-13 21:16:59 +0200

    first draft for "MetaNode" a.k.a. FlatMap or >>=
    as yet untested

---
+++ metanode.go
@@ -0,0 +1,49 @@
+package lazyexp
+
+import (
+	"context"
+	"sync"
+	"sync/atomic"
+)
+
+// NewMetaNode creates a new node that itself yields nodes that are fetched when it is fetched.
+func NewMetaNode(dependencies Dependencies, fetch func(context.Context, []error) (Node, error)) Node {
+	return &metaNode{
+		fetcher:      fetch,
+		dependencies: dependencies,
+	}
+}
+
+type metaNode struct {
+	fetcher      func(context.Context, []error) (Node, error)
+	dependencies Dependencies
+	once         sync.Once
+	result       Node
+	err          error
+	iFetched     int32
+}
+
+func (m *metaNode) Fetch(ctx context.Context) error { return m.fetch(ctx, false) }
+
+func (m *metaNode) FetchStrict(ctx context.Context) error { return m.fetch(ctx, true) }
+
+func (m *metaNode) fetch(ctx context.Context, strict bool) error {
+	m.once.Do(func() {
+		m.err = newNode(m.dependencies, func(subCtx context.Context, errs []error) error {
+			var err error
+			m.result, err = m.fetcher(subCtx, errs)
+			return err
+		}).fetch(ctx, strict)
+		if m.err == nil {
+			m.err = m.result.Fetch(ctx)
+		}
+		atomic.StoreInt32(&m.iFetched, 1)
+	})
+	return m.err
+}
+
+func (m *metaNode) fetched() bool {
+	return atomic.LoadInt32(&m.iFetched) != 0
+}
+
+func (m *metaNode) noUserImplementations() {}
