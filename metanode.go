127c9d3f7863697a3dee210712c373f821edc1da
Willi Schinmeyer <willi.schinmeyer@freiheit.com>
2017-06-05 10:28:44 +0200

    simplify MetaNode logic

--- metanode.go
+++ metanode.go
@@ -2,25 +2,26 @@ package lazyexp
 
 import (
 	"sync"
-	"sync/atomic"
 )
 
 // NewMetaNode creates a new node that itself yields nodes that are fetched when it is fetched.
 func NewMetaNode(dependencies Dependencies, fetch func([]error) (Node, error)) Node {
-	return &metaNode{
-		fetcher:      fetch,
+	m := metaNode{
 		dependencies: dependencies,
 	}
+	m.fetcherNode = newNode(m.dependencies, func(errs []error) error {
+		var err error
+		m.result, err = fetch(errs)
+		return err
+	})
+	return &m
 }
 
 type metaNode struct {
-	fetcher      func([]error) (Node, error)
 	dependencies Dependencies
 	once         sync.Once
 	fetcherNode  *node
 	result       Node
-	err          error
-	iFetched     int32
 }
 
 func (m *metaNode) Fetch() error { return m.fetch(false) }
@@ -29,22 +30,21 @@ func (m *metaNode) FetchStrict() error { return m.fetch(true) }
 
 func (m *metaNode) fetch(strict bool) error {
 	m.once.Do(func() {
-		m.fetcherNode = newNode(m.dependencies, func(errs []error) error {
-			var err error
-			m.result, err = m.fetcher(errs)
-			return err
-		})
-		m.err = m.fetcherNode.fetch(strict)
-		if m.err == nil {
-			m.err = m.result.Fetch()
+		m.fetcherNode.fetch(strict)
+		if m.fetcherNode.err == nil {
+			m.result.Fetch()
 		}
-		atomic.StoreInt32(&m.iFetched, 1)
 	})
-	return m.err
+	if m.fetcherNode.err != nil {
+		return m.fetcherNode.err
+	}
+	return m.result.Fetch()
 }
 
 func (m *metaNode) fetched() bool {
-	return atomic.LoadInt32(&m.iFetched) != 0
+	return m.fetcherNode.fetched() &&
+		(m.fetcherNode.err != nil ||
+			m.result.fetched())
 }
 
 func (m *metaNode) noUserImplementations() {}
