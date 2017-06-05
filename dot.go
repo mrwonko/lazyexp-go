0de027aa2570fa5b77db968cf5cf370f6d9b6a2d
Willi Schinmeyer <willi.schinmeyer@freiheit.com>
2017-06-05 12:50:57 +0200

    dot serialization

---
+++ dot.go
@@ -0,0 +1,62 @@
+package lazyexp
+
+import (
+	"io"
+	"strings"
+	"text/template"
+)
+
+func dotEscapeLabel(label string) string {
+	return strings.NewReplacer(
+		`\`, `\\`,
+		"\n", `\n`,
+		`"`, `\"`,
+	).Replace(label)
+}
+
+func isChild(id ID) bool {
+	return id != NoChild
+}
+
+func discarded(err error) bool {
+	return err == Discarded
+}
+
+var dotTemplate = template.Must(template.New("dot").Funcs(
+	template.FuncMap{
+		"escapeLabel": dotEscapeLabel,
+		"isChild":     isChild,
+		"discarded":   discarded,
+	}).Parse(`digraph {
+{{- range $id, $node := .}}
+	node{{$id}} [label="{{$node.Description | escapeLabel}}"
+		{{- if $node.Evaluated -}}
+			{{- if $node.Err -}}
+				,color=red
+			{{- else -}}
+				,color=green
+			{{- end -}}
+		{{- end -}}
+	];
+{{- range $dep := $node.Dependencies}}
+	node{{$dep.ID}} -> node{{$id}} [
+		{{- if discarded $dep.Err -}}
+			color=grey
+		{{- else if $dep.Err -}}
+			color=red
+		{{- else -}}
+			color=green
+		{{- end -}}
+	];
+{{- end}}
+{{- if isChild $node.Child}}
+	node{{$node.Child}} -> node{{$id}} [style=dotted];
+{{- end}}
+{{- end}}
+}
+`))
+
+// Dot serializes the given FlattenedNodes to the GraphViz Dot format.
+func Dot(wr io.Writer, fn FlattenedNodes) error {
+	return dotTemplate.Execute(wr, fn)
+}
